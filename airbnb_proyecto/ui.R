cd9576591e16f17beabcd78f511af0f4dbbdf37f
Javier Alejandro Mazariegos Godoy <javiermazariegos@ufm.edu>
2022-10-03 16:53:47 -0600

    leer el archivo

--- airbnb_proyecto/ui.R
+++ airbnb_proyecto/ui.R
@@ -1,13 +1,8 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(readr)
+
+df <- read_csv("Airbnb_Open_Data.csv")
+df <- df[1:25]
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
