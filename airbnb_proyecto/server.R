8f7bac573e2a8e1c4ee0da80fca457056f595196
Javier Alejandro Mazariegos Godoy <javiermazariegos@ufm.edu>
2022-10-04 12:22:10 -0600

    ejemplo de tabla con filtro

--- airbnb_proyecto/server.R
+++ airbnb_proyecto/server.R
@@ -1,19 +1,12 @@
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+    output$tabla <- DT::renderDataTable({
+    subset(df, df$`review rate number` == input$inReview) %>% DT::datatable()
     })
 
 })
--- airbnb_proyecto/ui.R
+++ airbnb_proyecto/ui.R
@@ -1,28 +1,28 @@
 library(shiny)
 library(readr)
+library(dplyr)
 
 df <- read_csv("Airbnb_Open_Data.csv")
 df <- df[1:25]
+df <- na.omit(df)
+
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+shinyUI(
+  
+  fluidPage(
+  
+  titlePanel("Tabla airbnb"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("inReview","Select review rate",sort(unique(df$`review rate number`)))
+    ),
+    mainPanel(
+      DT::dataTableOutput("tabla")
     )
-))
+    
+  )
+  
+)
+
+)
