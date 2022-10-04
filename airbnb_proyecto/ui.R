e22fbaee34bb1cfcc0c54ae01d5c9176a1576e9e
Javier Alejandro Mazariegos Godoy <javiermazariegos@ufm.edu>
2022-10-04 12:51:50 -0600

    tabla con filtros

--- airbnb_proyecto/ui.R
+++ airbnb_proyecto/ui.R
@@ -8,20 +8,34 @@ df <- na.omit(df)
 
 
 # Define UI for application that draws a histogram
-shinyUI(
+shinyUI(fluidPage(
   
-  fluidPage(
-  
-  titlePanel("Tabla airbnb"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("inReview","Select review rate",sort(unique(df$`review rate number`)))
-    ),
-    mainPanel(
-      DT::dataTableOutput("tabla")
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Tabla", icon = icon("table"), value = "table", 
+               mainPanel(
+                 width = 12, style="margin-left:0.5%; margin-right:0.5%",
+                       fluidRow(
+                         p("Filtros", 
+                           style = "font-weight: bold; color: black;"),
+                         p("Seleccione al menos un filtro para mostrar la tabla. "),
+                         br()
+                       ),
+                       fluidRow(
+                         selectInput("inReview","Select review rate",sort(unique(df$`review rate number`)))
+                       ),
+                      fluidRow(
+                        div(DT::dataTableOutput("tabla")),
+                        style = "font-size: 98%; width: 98%"
+                      )
+               )
+      ),
+      tabPanel("Graficas", verbatimTextOutput("summary")),
+      tabPanel("Mapas", tableOutput("table"))
     )
-    
   )
+  
+  
   
 )
 
