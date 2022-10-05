61455b0d3efbae04197e7dbc89f91f637e58adff
Javier Alejandro Mazariegos Godoy <javiermazariegos@ufm.edu>
2022-10-04 19:11:08 -0600

    mejora en visualización de la tabla

--- airbnb_proyecto/server.R
+++ airbnb_proyecto/server.R
@@ -6,7 +6,26 @@ library(ggplot2)
 shinyServer(function(input, output) {
 
     output$tabla <- DT::renderDataTable({
-    subset(df, df$`review rate number` == input$inReview) %>% DT::datatable()
+        tabla <- df
+        if(input$inReview != ""){
+          if(input$inReview == "Todos"){
+            tabla <- tabla 
+          }
+          else{
+            tabla <- tabla %>% filter(`review rate number` %in% input$inReview) 
+          }
+        }
+        if(!is.null(input$inVerificacion)){
+          if(input$inVerificacion == "ambos"){
+            tabla <- tabla 
+          }
+          else{
+          tabla <- tabla %>% filter(host_identity_verified %in% input$inVerificacion)
+          }
+        }
+         
+        tabla <- tabla %>% select(NAME,`review rate number`,host_identity_verified,`host name`,`neighbourhood group`,`room type`,price,`availability 365`) %>%
+        DT::datatable(options = list(searching=FALSE,bLengthChange =FALSE))
     })
 
 })
--- airbnb_proyecto/ui.R
+++ airbnb_proyecto/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyWidgets)
 library(readr)
 library(dplyr)
 
@@ -9,24 +10,32 @@ df <- na.omit(df)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  mainPanel(
+  mainPanel( width=100,
     tabsetPanel(
       tabPanel("Tabla", icon = icon("table"), value = "table", 
                mainPanel(
                  width = 12, style="margin-left:0.5%; margin-right:0.5%",
                        fluidRow(
+                         br(),
                          p("Filtros", 
                            style = "font-weight: bold; color: black;"),
                          p("Seleccione al menos un filtro para mostrar la tabla. "),
                          br()
                        ),
                        fluidRow(
-                         selectInput("inReview","Select review rate",sort(unique(df$`review rate number`)))
+                         column(3,
+                                radioGroupButtons(
+                                  inputId = "inVerificacion",
+                                  label = "Verificación de anfitrion: ",
+                                  choices = c("ambos","verified", "unconfirmed"),
+                                  selected = character(0)
+                                ),
+                                selectInput("inReview","Select review rate: ",c("Todos",sort(unique(df$`review rate number`))))
+                         )
                        ),
                       fluidRow(
                         div(DT::dataTableOutput("tabla")),
-                        style = "font-size: 98%; width: 98%"
+                        style = "font-size: 98%; width: 100%"
                       )
                )
       ),
